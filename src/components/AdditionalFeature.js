bd35b8b26486cd731aceb539e249d6f74822f43b
Mac <xuromeo@gmail.com>
2020-05-20 21:07:30 -0700

    finish on 520

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,25 @@
 import React from 'react';
-
+import {connect} from 'react-redux';
+import {addFeatures} from './action';
 const AdditionalFeature = props => {
+  console.log('additional',props);
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button onClick={()=>props.addFeatures(props.feature)} className="button">Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
+const mapStateToProps = state =>{
+  return {
+    additionalPrice:state.additionalPrice,
+    car:state.car,
+    additionalFeatures:state.additionalFeatures
+  }
+}
+export default connect(
+  mapStateToProps,
+  { addFeatures }
+)(AdditionalFeature);
 
-export default AdditionalFeature;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { Provider } from "react-redux";
 import 'bulma/css/bulma.css';
 import './styles.scss';
+import { createStore } from "redux";
+import {carReducer} from './components/carReducer';
 
+const store = createStore(carReducer);
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store = {store}><App /></Provider>, rootElement);
